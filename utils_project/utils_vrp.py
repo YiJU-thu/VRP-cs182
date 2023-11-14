7bf24cccde77814790b16c462d95c68d3b4e8cb1
Yi Ju <juyi9710@gmail.com>
2023-11-14 01:13:43 -0800

    vrp utils

--- utils_project/utils_vrp.py
+++ utils_project/utils_vrp.py
@@ -36,7 +36,6 @@ def tour_len(tour, coords=None, dist_mat=None, norm=None):
         if norm == "Linf":
             return np.sum(np.max(np.abs(v1 - v), axis=1))
 
-
 def tour_len_batch(tour, coords=None, dist_mat=None, norm=None):
     """
     Compute the length of a batch of instances. (I=batch size)
@@ -53,9 +52,29 @@ def tour_len_batch(tour, coords=None, dist_mat=None, norm=None):
     raise NotImplementedError
 
 
-def normalize_dist_mat(coords, dist_mat, norm="L2"):
+def normed_dist_mat(coords, norm="L2"):
     """
-    Normalize the distance matrix, i.e., how much the distance matrix deviates from 
+    Compute the normed distance matrix (i.e., the distance matrix when the norm is "L2").
+    """
+    assert coords.shape[1] == 2
+    assert norm in ["L1", "L2", "Linf"], "Invalid norm"
+    expand = coords[:, None, :] - coords[None, :, :]
+    if norm == "L2":
+        norm_dist_mat = np.sqrt((expand ** 2).sum(axis=2))
+    if norm == "L1":
+        norm_dist_mat = np.abs(expand).sum(axis=2)
+    if norm == "Linf":
+        norm_dist_mat = np.max(np.abs(expand), axis=2)
+    return norm_dist_mat
+
+def normed_dist_mat_batch(coords, norm="L2"):
+    raise NotImplementedError
+
+
+def rel_dist_mat(coords, dist_mat, norm="L2"):
+    """
+    Normalize the distance matrix relative to a simple normed distance, 
+    i.e., how much the distance matrix deviates from 
         the one when the distance is a norm (e.g., "L2"), which info has effectively embedded in coords.
     
     args:
@@ -85,8 +104,60 @@ def normalize_dist_mat(coords, dist_mat, norm="L2"):
     k = np.dot(x, y) / np.dot(x, x)
 
     dist_mat_hat = k * norm_dist_mat
-    dist_mat_normalized = np.divide(dist_mat, dist_mat_hat, out=np.zeros_like(dist_mat), where=dist_mat_hat!=0)
+    dist_mat_rel = np.divide(dist_mat, dist_mat_hat, out=np.zeros_like(dist_mat), where=dist_mat_hat!=0)
 
-    return dist_mat_normalized
+    return dist_mat_rel
 
 
+def normalize_dist_mat(dist_mat):
+    """
+    Normalize the distance matrix by dividing the max distance.
+    """
+    assert dist_mat.shape[0] == dist_mat.shape[1]
+    l, u = dist_mat.min(), dist_mat.max()
+    eps = 1e-3
+    return (dist_mat-l) / max(u-l, eps)
+
+def normalize_dist_mat_batch(dist_mat):
+    raise NotImplementedError
+
+
+def normalize_coords(coords):
+    """
+    rescale the coords into the unit square [0,1] x [0,1]
+    """
+
+    assert coords.shape[1] == 2
+    coords_min = coords.min(axis=0)
+    coords_max = coords.max(axis=0)
+    scale = coords_max.max() - coords_min.min() 
+    # scale is a scalar: must apply the same factor to both x & y, so that the optimal tour is not changed
+    coords_normalized = (coords - coords_min) / scale
+
+    # by doing this, we make sure that coords are in [0,1] x [0,1]
+    # however, if the original data have larger range on x or y, 
+    # then the normalized data are within a very narrow range, e.g., [0, 0.1] x [0, 1]
+    # FIXME: should we do another way, e.g., scale to roughly [0,0.3] x [0,3]
+    # FIXME: another case is usually, the [station] is an outlier, large spaces are empty
+
+    return coords_normalized
+
+def normalize_coords_batch(coords):
+    raise NotImplementedError
+
+
+def random_graph(N, I, dist_mat_params, seed=None):
+    """
+    Generate random coordinates in the unit square [0,1] x [0,1]
+    """
+    # in training, we should NOT use the same seed for all instances / batchs / epoches ...
+    # for reproducibility, set the seed OUTSIDE (for the entire training process)
+    if seed is not None:    
+        np.random.seed(seed)
+    coords = np.random.uniform(size=(I, N, 2))
+    rel_dist_mat = ...  # TODO: what is the reasonable distribution to draw from?
+    raise NotImplementedError
+
+    dist_mat = rel_dist_mat * normed_dist_mat(coords, norm="L2")
+    return coords, rel_dist_mat, dist_mat
+    
