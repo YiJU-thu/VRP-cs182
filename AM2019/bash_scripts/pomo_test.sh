c9ccf5c243797f1b04d5ba68475a901c1f2083f7
YiJU-thu <juyi9710@gmail.com>
2024-01-03 01:02:29 -0800

    add pomo test script

---
+++ AM2019/bash_scripts/pomo_test.sh
@@ -0,0 +1,5 @@
+# python run.py --problem tsp --graph_size 20 --val_size 512 --epoch_size 5120 --baseline pomo --pomo_sample 8 --log_step 5 --run_name test_pomo --no_wandb
+# python run.py --problem tsp --graph_size 20 --val_size 512 --epoch_size 5120 --baseline pomo --rot_sample 8 --log_step 5 --run_name test_rot --no_wandb
+# python run.py --problem tsp --graph_size 20 --val_size 512 --epoch_size 5120 --baseline pomo --pomo_sample 4 --rot_sample 2 --log_step 5 --run_name test_pomo_rot --no_wandb
+# python run.py --problem tsp --graph_size 20 --non_Euc --val_size 512 --epoch_size 5120 --baseline pomo --pomo_sample 4 --rot_sample 2 --log_step 5 --run_name test_pomo_rot --no_wandb
+python run.py --problem tsp --graph_size 20 --val_size 512 --epoch_size 5120 --log_step 5 --run_name test_no_pomo --no_wandb
