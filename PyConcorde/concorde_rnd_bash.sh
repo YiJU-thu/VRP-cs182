76521badfc95f6662034612d595a18353353df63
YiJU-thu <juyi9710@gmail.com>
2024-04-04 07:43:05 -0700

    test dask bash

--- PyConcorde/concorde_rnd_bash.sh
+++ PyConcorde/concorde_rnd_bash.sh
@@ -1,7 +1,9 @@
 data_dir="/home/ecal_team/datasets/amazon_vrp/amazon_processed"
-python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N20_I1000_C_seed1234_iter4.pkl \
- --redo_failed --out_prefix CCD --clear 100 --save 100 --scale 1000 --big_M 50 --log CCD_rnd_20.log --recover_graph
-python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N50_I1000_C_seed1234_iter4.pkl\
- --redo_failed --out_prefix CCD --clear 10 --save 10 --scale 1000 --big_M 50 --log CCD_rnd_50.log --recover_graph
-python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N100_I1000_C_seed1234_iter4.pkl\
- --redo_failed --out_prefix CCD --clear 1 --save 1 --scale 1000 --big_M 50 --log CCD_rnd_100.log --recover_graph
+python concorde_solve.py --dask_parallel --type ATSP --data_dir $data_dir --data rnd_N20_I1000_C_seed1234_iter4.pkl \
+ --redo_failed --out_prefix TESt_Dask --clear 10 --save 10 --scale 1000 --big_M 50 --log CCD_rnd_20.log --recover_graph
+# python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N20_I1000_C_seed1234_iter4.pkl \
+#  --redo_failed --out_prefix CCD --clear 100 --save 100 --scale 1000 --big_M 50 --log CCD_rnd_20.log --recover_graph
+# python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N50_I1000_C_seed1234_iter4.pkl\
+#  --redo_failed --out_prefix CCD --clear 10 --save 10 --scale 1000 --big_M 50 --log CCD_rnd_50.log --recover_graph
+# python concorde_solve.py --type ATSP --data_dir $data_dir --data rnd_N100_I1000_C_seed1234_iter4.pkl\
+#  --redo_failed --out_prefix CCD --clear 1 --save 1 --scale 1000 --big_M 50 --log CCD_rnd_100.log --recover_graph
