35d2c291441213fc28f5a1d404305bc0a4f2bc57
YiJU-thu <juyi9710@gmail.com>
2024-04-08 23:29:50 -0700

    make EUC, & ATSP-C datasets

--- dataset/nE2024_data/SL_100/make_SL_ds.sh
+++ dataset/nE2024_data/SL_100/make_SL_ds.sh
@@ -1,7 +1,14 @@
 # run this under dataset directory
-save_dir="nE2024_data/test"
+save_dir="nE2024_data/SL_100"
+
+# Simple-dist Euc TSP test dataset
+num_graphs=100000
+python make_rnd_dataset.py --Euc --graph_size 100 --num_graphs $num_graphs --seed 101 --save_dir $save_dir
+
+# Simple-dist non-Euc TSP test dataset
 num_graphs=5000
 for ((i=101; i<=120; i++))
 do
     python make_rnd_dataset.py --graph_size 100 --num_graphs $num_graphs --seed $i --save_dir $save_dir --force_triangle_iter 4
 done
+
