4e05885aa1de1e615fc3e35184b8a012b82141e4
YiJU-thu <juyi9710@gmail.com>
2024-05-16 04:59:10 -0700

    update data gen / solve bash

--- PyConcorde/ccd_test_atsp.sh
+++ PyConcorde/ccd_test_atsp.sh
@@ -1,4 +1,10 @@
 data_dir="../dataset/nE2024_data/test"
+
+# MatNet style ATSP
+python concorde_solve.py --dask_parallel --type ATSP --data_dir $data_dir --out_dir $data_dir --data rnd_N100_I10000_NC_seed0_iter4_NoTrack.pkl \
+    --redo_failed --out_prefix CCD --clear 10 --save 10 --scale 1 --big_M 100 --log CCD_rnd_100.log --n_workers 10
+
+
 for ((i=0; i<=1; i++))
 do
     python concorde_solve.py --dask_parallel --type ATSP --data_dir $data_dir --out_dir $data_dir --data rnd_N100_I5000_S_seed${i}_iter4_NoTrack.pkl \
